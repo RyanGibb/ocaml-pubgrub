93182f0f73133bd2391d4a82d50e2bf25476f370
Ryan Gibb <ryan@freumh.org>
2026-07-18 15:05:19 +0100

    index incompatibilities by name

--- lib/incompatibilities.ml
+++ lib/incompatibilities.ml
@@ -1,16 +1,36 @@
 module Make (N : Types.NAME) (V : Types.VERSION) = struct
   include Types.Make (N) (V)
 
-  type t = incompatibility list
+  module NameMap = Map.Make (struct
+    type t = name
 
-  let empty = []
-  let add incomp t = incomp :: t
+    let compare = compare_name
+  end)
 
-  let find_for_name n t =
-    List.filter
-      (fun incomp ->
-        List.exists (fun tm -> compare_name (term_name tm) n = 0) incomp.terms)
-      t
+  type t = incompatibility list NameMap.t
 
-  let mem incomp t = List.exists (fun i' -> equal_terms i'.terms incomp.terms) t
+  let empty = NameMap.empty
+
+  (* Unique names mentioned by an incompatibility's terms. *)
+  let incomp_names incomp =
+    List.fold_left
+      (fun acc t ->
+        let n = term_name t in
+        if List.exists (fun n' -> compare_name n' n = 0) acc then acc else n :: acc)
+      [] incomp.terms
+
+  let add incomp t =
+    List.fold_left
+      (fun m n ->
+        let existing = NameMap.find_opt n m |> Option.value ~default:[] in
+        NameMap.add n (incomp :: existing) m)
+      t (incomp_names incomp)
+
+  let find_for_name n t = NameMap.find_opt n t |> Option.value ~default:[]
+
+  let mem incomp t =
+    match incomp_names incomp with
+    | [] -> false
+    | first :: _ ->
+        List.exists (fun i' -> equal_terms i'.terms incomp.terms) (find_for_name first t)
 end
